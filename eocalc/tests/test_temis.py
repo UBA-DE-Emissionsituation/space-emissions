24f4f7e6541110e480d0a18b134c24d360b3f279
Kevin Hausmann <kevin.hausmann@uba.de>
2021-04-20 21:39:34 +0200

    Add more tests for basic methods

--- eocalc/tests/test_temis.py
+++ eocalc/tests/test_temis.py
@@ -2,17 +2,35 @@
 import unittest
 from datetime import date, timedelta
 
+from shapely.geometry import shape
+
 from eocalc.context import Pollutant
 from eocalc.methods.temis import TEMISTropomiMonthlyMeanNOxEmissionCalculator
 
 
 class TestTEMISTropomiMonthlyMeanNOxMethods(unittest.TestCase):
 
-    def test_end_date(self):
-        now = date.fromisoformat("2021-04-19")
-        end = date.fromisoformat("2021-02-28")
+    def test_covers(self):
+        calc = TEMISTropomiMonthlyMeanNOxEmissionCalculator()
+        north = shape({'type': 'MultiPolygon',
+                      'coordinates': [[[[-110., 20.], [140., 20.], [180., 40.], [-180., 30.], [-110., 20.]]]]})
+        south = shape({'type': 'MultiPolygon',
+                       'coordinates': [[[[-110., -20.], [140., -20.], [180., -40.], [-180., -30.], [-110., -20.]]]]})
+        both = shape({'type': 'MultiPolygon',
+                       'coordinates': [[[[-110., 20.], [140., -20.], [180., -40.], [-180., -30.], [-110., 20.]]]]})
+        self.assertTrue(calc.covers(north))
+        self.assertTrue(calc.covers(south))
+        self.assertTrue(calc.covers(both))
 
-        self.assertEqual(end, (now.replace(day=1)-timedelta(days=1)).replace(day=1)-timedelta(days=1))
+    def test_end_date(self):
+        test = date.fromisoformat("2021-04-19")
+        self.assertEqual(date.fromisoformat("2021-02-28"), (test.replace(day=1)-timedelta(days=1)).replace(day=1)-timedelta(days=1))
+
+        test = date.fromisoformat("2021-01-01")
+        self.assertEqual(date.fromisoformat("2020-11-30"), (test.replace(day=1)-timedelta(days=1)).replace(day=1)-timedelta(days=1))
+
+        test = date.fromisoformat("2021-05-31")
+        self.assertEqual(date.fromisoformat("2021-03-31"), (test.replace(day=1)-timedelta(days=1)).replace(day=1)-timedelta(days=1))
 
     def test_supports(self):
         for p in Pollutant:
