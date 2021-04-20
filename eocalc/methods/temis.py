4191b98c7688c124f886eb4c967cdcf8ad71de9b
Kevin Hausmann <kevin.hausmann@uba.de>
2021-04-20 21:40:54 +0200

    Create TEMIS grid overlay for given region, create GeoDataFrame and put some data

--- eocalc/methods/temis.py
+++ eocalc/methods/temis.py
@@ -4,6 +4,7 @@
 from datetime import date, timedelta
 
 from shapely.geometry import MultiPolygon, shape
+import geopandas
 
 from eocalc.context import Pollutant
 from eocalc.methods.base import EOEmissionCalculator, DateRange
@@ -37,4 +38,36 @@ class TEMISTropomiMonthlyMeanNOxEmissionCalculator(EOEmissionCalculator):
         return pollutant == Pollutant.NOx
 
     def run(self, region: MultiPolygon, period: DateRange, pollutant: Pollutant) -> dict:
-        return {}
+
+        grid = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        bin_width = 0.125
+        min_long = region.bounds[0] - region.bounds[0] % bin_width
+        min_lat = region.bounds[1] - region.bounds[1] % bin_width
+        max_long = region.bounds[2] + bin_width - (region.bounds[2] % bin_width)
+        max_lat = region.bounds[3] + bin_width - (region.bounds[3] % bin_width)
+        for long in (min_long + x * bin_width for x in range(int((max_long - min_long) / bin_width) + 1)):
+            for lat in (min_lat + y * bin_width for y in range(int((max_lat - min_lat) / bin_width) + 1)):
+                grid["features"].append({
+                    "type": "Feature",
+                    "properties": {"center": f"{lat + bin_width / 2}/{long + bin_width / 2}"},
+                    "geometry": {"type": "Polygon", "coordinates": [
+                        [(long, lat), (long + bin_width, lat), (long + bin_width, lat + bin_width),
+                         (long, lat + bin_width), (long, lat)]]},
+                })
+
+        grid_result = geopandas.GeoDataFrame.from_features(grid, crs="EPSG:4326")
+
+        for day in range((period.end - period.start).days + 1):
+            grid_result[f"{period.start + timedelta(days=day)}"] = day
+
+        grid_result["SUM"] = grid_result.sum(axis=1, numeric_only=True)
+        grid_result = geopandas.overlay(grid_result, geopandas.GeoDataFrame({'geometry': [region]}, crs="EPSG:4326"), how='intersection')
+
+        return {
+            EOEmissionCalculator.TOTAL_EMISSIONS_KEY: grid_result["SUM"].sum(),
+            EOEmissionCalculator.GRIDDED_EMISSIONS_KEY: grid_result
+        }
