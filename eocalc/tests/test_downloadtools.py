4e4f56b1d92167662f912b7ade02c3887d40381b
jptokaya <janot.tokaya@tno.nl>
2021-08-13 07:23:52 +0000

    update some s5p tests

--- eocalc/tests/test_downloadtools.py
+++ eocalc/tests/test_downloadtools.py
@@ -5,8 +5,8 @@ import inspect
 
 from requests import HTTPError
 
-from eocalc.methods.base import DateRange
-from eocalc.tools.downloadtools import input_check_period,input_check_path,input_check_era_lvl,era5_download
+from eocalc.methods.base import DateRange, Pollutant
+from eocalc.tools.downloadtools import input_check_period,input_check_path,input_check_era_lvl,era5_download,satellite_download
 
 class TestInputs:
     def test_invalid_period(self):
@@ -59,21 +59,25 @@ class TestInputs:
         assert ["123.45"] == input_check_era_lvl(['123.45'])
         assert ["123.45"] == input_check_era_lvl([123.45])
 
-class TestDownload:
-    def bad_connect_s5p_download(self):
-        with pytest.raises(Exception):
-            era5_download(period = DateRange(end="2016-01-03", start="2016-01-02"),apikey = '123', replace=False)
-        with pytest.raises(Exception) as excinfo:
-            era5_download(period = DateRange(end="2016-01-03", start="2016-01-02"),apikey = '123', replace=False)
-        assert "401" in str(excinfo.value) #bad api
+    def test_invalid_input_satellite(self):
+        with pytest.raises(ValueError):
+            satellite_download(period = DateRange(end="2100-01-03", start="2100-01-02"), species = Pollutant.NO2, producttype = "L2__NO2___", satellite = "s2",replace =False)
 
+class TestDownload:
     def bad_connect_era_download(self):
         with pytest.raises(Exception):
-            era5_download(period = DateRange(end="2016-01-03", start="2016-01-02"),apikey = '123', replace=False)
+            era5_download(period = DateRange(end="2016-01-03", start="2016-01-02"), replace=False)
         with pytest.raises(Exception) as excinfo:
             era5_download(period = DateRange(end="2016-01-03", start="2016-01-02"),apikey = '123', replace=False)
-        assert "401" in str(excinfo.value) #bad api
+        assert "401" in str(excinfo.value) #bad api connection
 
+    def bad_request_s5p_download(self):
+        with pytest.raises(Exception) as excinfo:
+            satellite_download(period = DateRange(end="2100-01-03", start="2100-01-02"), species = Pollutant.NO2, producttype = "L2__NO2___", satellite = "s5p",replace =False)
+        assert "SentinelAPIError" in str(excinfo.value)
 
     def succesful_test_era_download(self):
         assert None == era5_download(period = DateRange(end="2016-01-03", start="2016-01-02"), replace=False)
+
+    def succesful_test_s5p_download(self):
+        assert None == satellite_download(period = DateRange(end="2016-01-03", start="2016-01-02"), species = Pollutant.NO2, producttype = "L2__NO2___", satellite = "s5p",replace =False)
